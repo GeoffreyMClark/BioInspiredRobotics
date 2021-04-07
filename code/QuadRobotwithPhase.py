b14d92d9a4d76ab2428cda1ccd99e635fb3ee503
Geoffrey Clark <42682271+GeoffreyMClark@users.noreply.github.com>
2021-04-07 10:35:48 -0700

    updated robot walking code again

--- code/QuadRobotwithPhase.py
+++ code/QuadRobotwithPhase.py
@@ -1,5 +1,6 @@
 import time
 import numpy as np
+from scipy import signal as scp
 import pybullet as p
 import pybullet_data as pd
 import time
@@ -29,9 +30,9 @@ class control_class:
 	target_foot_position = np.array([[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]])
 	target_angle = np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
 	torque = np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
-	m = np.array([1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0])
-	b = np.array([1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0])
-	k = np.array([15.0,15.0,15.0,15.0,15.0,15.0,15.0,15.0,15.0,15.0,15.0,15.0,15.0,15.0,15.0,15.0])
+	m = np.array([1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5,1.5])
+	b = np.array([.017,.017,.017,.017,.017,.017,.017,.017,.017,.017,.017,.017,.017,.017,.017,.017])
+	k = np.array([1.2,1.2,1.2,1.2,1.2,1.2,1.2,1.2,1.2,1.2,1.2,1.2,1.2,1.2,1.2,1.2])
 
 
 def add_robot():
@@ -94,36 +95,70 @@ def impedance_control(robot, robot_state, control_state, numJoints):
 	jvel = robot_state.joint_vels
 	jacc = robot_state.joint_accs
 	#calculate torque based on previous values then return torque
-	torque = -control_state.m*jacc - control_state.b*jvel - control_state.k*(jpos-tar)
+	torque = -(control_state.m*jacc + control_state.b*jvel + control_state.k*(jpos-tar))
 	robot_torque_control(robot, numJoints, torque)
 
 def movement_controller(robot, robot_state, control_state, numJoints):
 	# Standing 
-	if control_state.action==0
-		control_state.target_foot_position = np.array([[0.0,0.25,0.0],[0.0,0.25,0.0],[0.0,0.25,0.0],[0.0,0.25,0.0]])
+	if control_state.action==0:
+		pos_y = np.clip(control_state.t_step/5,.1,.3)
+		print(pos_y)
+		control_state.target_foot_position = np.array([[0.0,pos_y,0.0],[0.0,pos_y,0.0],[0.0,pos_y,0.0],[0.0,pos_y,0.0]])
 		new_angles = model_ik(control_state.target_foot_position,training=False).numpy()
 		control_state.target_angle = np.concatenate((new_angles,np.zeros((4,1))),axis=1).flatten()
 		impedance_control(robot, robot_state, control_state, numJoints)
 
-	# hobble
-	elif control_state.action==1
-		leg1x = np.cos(control_state.dt)
-		leg1z = np.sin(control_state.dt)+.25
-		leg2x = np.cos(control_state.dt+.25)
-		leg2z = np.sin(control_state.dt+.25)+.25
-		leg3x = np.cos(control_state.dt+.50)
-		leg3z = np.sin(control_state.dt+.50)+.25
-		leg4x = np.cos(control_state.dt+.75)
-		leg4z = np.sin(control_state.dt+.75)+.25
-
+	# Hobble (sinusoids) - doesnt work
+	elif control_state.action==1:
+		# phase offsets
+		leg1_off=0.0; leg2_off=1.0; leg3_off=0.5; leg4_off=1.5
+		speed = np.pi
+		leg1x = .1*   np.cos(speed*control_state.t_step + speed*leg1_off)
+		leg1z = np.clip(.05* np.sin(speed*control_state.t_step + speed*leg1_off) + .25, 0, .25)
+		leg2x = .1*   np.cos(speed*control_state.t_step + speed*leg2_off)
+		leg2z = np.clip(.05* np.sin(speed*control_state.t_step + speed*leg2_off) + .25, 0, .25)
+		leg3x = .1*   np.cos(speed*control_state.t_step + speed*leg3_off)+.025
+		leg3z = np.clip(.05* np.sin(speed*control_state.t_step + speed*leg3_off) + .25, 0, .25)
+		leg4x = .1*   np.cos(speed*control_state.t_step + speed*leg4_off)+.025
+		leg4z = np.clip(.05* np.sin(speed*control_state.t_step + speed*leg4_off) + .25, 0, .25)
 		control_state.target_foot_position = np.array([[0.0,leg1z,leg1x],[0.0,leg2z,leg2x],[0.0,leg3z,leg3x],[0.0,leg4z,leg4x]])
 		new_angles = model_ik(control_state.target_foot_position,training=False).numpy()
 		control_state.target_angle = np.concatenate((new_angles,np.zeros((4,1))),axis=1).flatten()
 		impedance_control(robot, robot_state, control_state, numJoints)
-	pass
+	
+	# Walking
+	elif control_state.action==2:
+		# seconds/stride | step duty cycle | Stride length | step height
+		sps=1; DC=0.85; SL=.2; SH=.1
+		# phase offsets
+		leg_off=np.array([0.0,0.5,0.25,0.75])
+		stance=np.array([False,False,False,False])
+		leg_phase=np.array([0.0,0.0,0.0,0.0])
+		leg_z=np.array([0.0,0.0,0.0,0.0])
+		leg_x=np.array([0.0,0.0,0.0,0.0])
+		phase = (scp.sawtooth(control_state.t_step*(2*np.pi)/sps)+1)/2
+		for i in range(4):
+			leg_phase[i]=(scp.sawtooth((control_state.t_step*(2*np.pi/sps)+leg_off[i]*(2*np.pi)))+1)/2
+			if leg_phase[i]<DC: 
+				stance[i] = True
+				leg_x[i] = SL/2 - SL*(leg_phase[i]/DC)
+				leg_z[i] = 0.3
+			else: 
+				stance[i] = False
+				leg_x[i] = -SL/2 + SL*((leg_phase[i]-DC)/(1-DC))
+				leg_z[i] = -SH*np.sin(((leg_phase[i]-DC)/(1-DC))*np.pi)+.3
+
+		control_state.target_foot_position = np.array([[0.02,leg_z[0],leg_x[0]+.02],[-0.02,leg_z[1],leg_x[1]+.02],[0.02,leg_z[2],leg_x[2]+.08],[-0.02,leg_z[3],leg_x[3]+.08]])
+		new_angles = model_ik(control_state.target_foot_position,training=False).numpy()
+		control_state.target_angle = np.concatenate((new_angles,np.zeros((4,1))),axis=1).flatten()
+		impedance_control(robot, robot_state, control_state, numJoints)
 
 
 
+
+def control_selector(control_state):
+	if control_state.t_step>4:
+		control_state.action=2
 
 
 
@@ -156,6 +191,7 @@ if __name__ =="__main__":
 		robot_data(robot, robot_state, dt)
 
 		# controller
+		control_selector(control_state)
 		movement_controller(robot, robot_state, control_state, numJoints)
 
 		# Take simulation steps
